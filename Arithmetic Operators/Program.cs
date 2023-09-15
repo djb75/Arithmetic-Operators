ba035456f8e6a8b8fe05069be3078eba42e5d457
David Jorge-Bates <jorgebd@stpaulsschool.org.uk>
2023-09-15 12:32:45 +0100

    Add project files.

---
+++ Arithmetic Operators/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Arithmetic_Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
